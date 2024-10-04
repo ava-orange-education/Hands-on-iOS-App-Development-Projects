cd28287a0b6020170b1f910c95c51db81583a1db
aish504 <aish.kodali@gmail.com>
2024-10-05 09:06:31 +1000

    Add files via upload

---
+++ Chapter - 5/TODO List/TODO List/TODOListItem.swift
@@ -0,0 +1,16 @@
+//
+//  TODOListItem.swift
+//  TODO List
+//
+//  
+//
+
+import Foundation
+
+struct TODOListItem {
+    var id: Int 
+    var item: String?
+    var notes: String?
+    var date: Date?
+    var time: Date?
+}
