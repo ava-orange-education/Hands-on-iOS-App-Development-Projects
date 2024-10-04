a0c67948478fa03270c77a784cfcae85ed314af2
aish504 <aish.kodali@gmail.com>
2024-10-05 09:01:11 +1000

    Chapter -01

---
+++ Chapter - 1/Basics/Basics/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Basics
+//
+
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var label: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        label.text = "Have a Great Day"
+    }
+
+
+}
+
